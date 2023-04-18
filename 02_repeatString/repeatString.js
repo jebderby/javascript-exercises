7c4a78d4706b2fcd18c9f61c01aaa2b58c39cee7
u411677 <jebderby@gmail.com>
2023-04-18 09:24:54 -0400

    Added code to solve problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+const repeatString = function(greeting, number) {
+
+// First need to check if the number is negative.
+if (number <0) {
+    return ('ERROR');
+    }
+
+// Second need to initialize a variable with an empty string.
+let string = "";
+
+// Third - Use a 'for' loop to repeat 'greeting' 'number' number of times
+for (let i = 0; i < number; i++) {
+    
+    // Add the 'greeting' to the 'string' variable on each iteration of the loop
+    string += greeting;
+}
+
+// After the loop has been completed, return the 'string' variable. 
+return string;
 
 };
 
